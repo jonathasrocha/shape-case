1ef90d2b9c99559016cc597359e307dbde06a621
Jonathas Rocha <jonathas.ro@hotmail.com>
2023-12-13 16:40:00 -0300

    add test

--- Makefile
+++ Makefile
@@ -25,3 +25,6 @@ etl:
 
 spark-sh:
 	docker exec -ti local-spark bash -c '$$SPARK_HOME/bin/pyspark --master local[*] --packages io.delta:$${DELTA_PACKAGE_VERSION},org.apache.hadoop:hadoop-aws:3.3.2 --conf spark.databricks.delta.retentionDurationCheck.enabled=false --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog --conf spark.hadoop.fs.s3a.access.key=minio --conf spark.hadoop.fs.s3a.secret.key=minio123 --conf spark.hadoop.fs.s3a.endpoint=http://minio:9000 --conf spark.hadoop.fs.s3a.region=us-east-1 --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem --conf spark.hadoop.fs.s3a.path.style.access=true'
+
+pytest:
+	docker exec -ti local-spark bash -c 'python3 -m pytest --log-cli-level info -p no:warnings -v ./equipment/tests'
--- equipment/tests/integration/test_equipment.py
+++ equipment/tests/integration/test_equipment.py
@@ -1,2 +1,47 @@
-def test_equipment():
-    pass
+from pyspark.sql import SparkSession
+import pytest
+from delta import configure_spark_with_delta_pip
+from equipment.pipelines.equipment import EquipmentETL
+
+
+@pytest.fixture
+def spark():
+    my_packages = [
+        "io.delta:delta-core_2.12:2.3.0",
+        "org.apache.hadoop:hadoop-aws:3.3.2",
+    ]
+     
+
+    builder = (
+        SparkSession.builder.appName("equipment")
+        .config("spark.executor.cores", "1")
+        .config("spark.executor.instances", "1")
+        .config("spark.sql.shuffle.partitions", "1")
+        .config(
+            "spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension"
+        )
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        )
+        .enableHiveSupport()
+    )
+
+    return configure_spark_with_delta_pip(
+        builder, extra_packages=my_packages
+    ).getOrCreate()
+    
+
+def test_equipment(spark):
+    
+
+    df_input = spark.createDataFrame(
+        [
+            ["[2021-05-18 0:20:48]	ERROR	sensor[5820]:	(temperature	311.29, vibration	6749.50)[2021-05-18 0:20:48]	ERROR	sensor[5820]:	(temperature	311.29, vibration	6749.50)"]
+        ],
+        ["value"]
+    )
+    equip = EquipmentETL()
+
+
+    assert len(equip.transform_equipment_failure_sensor(df_input).columns) == 1
