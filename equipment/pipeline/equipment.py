c1b64c48f6cc1aa96535fea79986a5418cbe2a21
Jonathas Rocha <jonathas.ro@hotmail.com>
2023-12-14 11:14:37 -0300

    add

--- equipment/pipeline/equipment.py
+++ equipment/pipeline/equipment.py
@@ -100,7 +100,7 @@ class EquipmentETL(StandardETL):
                 dim_equipment_latest,
                 (equipment_df.equipment_id == dim_equipment_latest.equipment_id)
                 & (
-                    col("dim_equipment_latest.updated_at_dt")
+                    dim_equipment_latest.updated_at_dt
                     < col("equipment_df.updated_at_dt")
                 ),
                 "leftanti",
