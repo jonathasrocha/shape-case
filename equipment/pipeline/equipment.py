fb4d1517e0547916a9f3ca1c3cff3beea259735d
Jonathas Rocha <jonathas.ro@hotmail.com>
2023-12-16 12:06:22 -0300

    fix

--- equipment/pipeline/equipment.py
+++ equipment/pipeline/equipment.py
@@ -56,14 +56,14 @@ class StandardETL(ABC):
     ) -> None:
         for input_dataset in input_datasets.values():
             if not input_dataset.skip_publish:
-                input_dataset.partition = input_dataset.partition if isinstance(input_dataset.partition, Column) else lit(input_dataset.partition)
+                partition_field = input_dataset.partition if isinstance(input_dataset.partition, Column) else lit(input_dataset.partition)
                 curr_data = input_dataset.curr_data.withColumn(
                     'etl_inserted', current_timestamp()
-                ).withColumn('partition', lit(input_dataset.partition))
+                ).withColumn('partition', input_dataset.partition)
                 if input_dataset.replace_partition:
                     curr_data.write.format("delta").mode("overwrite").option(
                         "replaceWhere",
-                        f"partition = '{input_dataset.partition}'",
+                        f"partition = '{partition_field._jc.toString()}'",
                     ).save(input_dataset.storage_path)
                 else:
                     targetDF = DeltaTable.forPath(
